d9f301bb035a004ca58806c2a726e20b8041e321
Keivon <keivonproctor@hotmail.com>
2019-08-20 09:51:01 +0100

    Updates

---
+++ precache-manifest.6caaff0f3ed7f6d2c9b7bdd70a8f5b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9087ace1c2e26d243b373fab359190",
+    "url": "/portfolio.git/index.html"
+  },
+  {
+    "revision": "8078a141f79e55da02d8",
+    "url": "/portfolio.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f80f0acbd2d0ce47e57c",
+    "url": "/portfolio.git/static/js/2.917ae229.chunk.js"
+  },
+  {
+    "revision": "8078a141f79e55da02d8",
+    "url": "/portfolio.git/static/js/main.8fce17ff.chunk.js"
+  },
+  {
+    "revision": "cd31de6de8d705768cd7",
+    "url": "/portfolio.git/static/js/runtime~main.1cd2d3fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
